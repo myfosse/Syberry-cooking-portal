4c0b72743a026ab8d88be6d520143b9ab46b5b8c
andrey.egorov <andrey.egorov.minsk@gmail.com>
2021-03-24 21:55:51 +0300

    Initial commit

---
+++ src/test/java/by/bsu/cookingportal/CookingportalApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsu.cookingportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingportalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
